71e3392d24f0e50ce3a3b732643220c9fc153ce7
Nicolas Allain <nicolas.manuel.allain@gmail.com>
2012-03-19 14:39:57 +0000

    Résolution de la variable faussée.

--- NNNA/NNNA/Graphics/Form/Control.cs
+++ NNNA/NNNA/Graphics/Form/Control.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using WindowsGame1;
+using NNNA;
 
 namespace NNNA.Form
 {
@@ -51,7 +51,7 @@ namespace NNNA.Form
             _textColor = Color.Silver;
             _visible = false;
             _backgroundColor = Color.White;
-            _background = new Texture2D(Game1.graphics.GraphicsDevice, 1, 1);
+            _background = new Texture2D(Game1._graphics.GraphicsDevice, 1, 1);
             _background.SetData(new Color[] { _backgroundColor });
         }
 
