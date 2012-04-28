bc75312825ec8a947c0d480df6f42208c2e844fa
Nicolas Mouton-Besson <nicmoutbee@hotmail.fr>
2012-04-28 17:39:40 +0000

    Usine.

---
+++ NNNA/NNNA/Physics/Batiment/Usine.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace NNNA
+{
+    class Usine : ProductionBuilding
+    {
+        // Ere 4
+        public Usine(int x = 0, int y = 0)
+            : base(x, y)
+        {
+            _cost.Add("Fer", 500);
+            _cost.Add("Pierre", 400);
+            _cost.Add("Or", 200);
+        }
+
+        public Usine(int x, int y, ContentManager content, Joueur joueur)
+            : base(x, y)
+        {
+            _joueur = joueur;
+            LoadContent(content, "Batiments/Usine1");
+            Life = 200;
+            LineSight = 128;
+            _cost.Add("Fer", 500);
+            _cost.Add("Pierre", 400);
+            _cost.Add("Or", 200);
+
+        }
+    }
+}
