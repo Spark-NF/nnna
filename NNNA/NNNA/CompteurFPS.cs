d0ad1ac7bafc067c12bcb1f2ff5cf454dc2293e3
Nicolas Allain <nicolas.manuel.allain@gmail.com>
2012-05-10 19:21:35 +0000

    Fix compteur de FPS.

--- NNNA/NNNA/CompteurFPS.cs
+++ NNNA/NNNA/CompteurFPS.cs
@@ -15,7 +15,7 @@ namespace NNNA
     public class CompteurFPS : Microsoft.Xna.Framework.GameComponent
     {
         public double FPS;
-        ulong _last;
+        int _now, _last, _ms;
         
         public CompteurFPS(Game game)
             : base(game)
@@ -26,21 +26,19 @@ namespace NNNA
         public override void Initialize()
         {
             FPS = 0.0d;
-            _last = ConvertToTotalMilliseconds(DateTime.Now);
+            _last = DateTime.Now.Millisecond;
+            _now = _last + 1;
+            _ms = 0;
             base.Initialize();
         }
 
         public override void Update(GameTime gameTime)
         {
-            ulong now = ConvertToTotalMilliseconds(DateTime.Now);
-            FPS = 1000.0d / (now - _last);
-            _last = now;
+            _now = DateTime.Now.Millisecond;
+            _ms = (_now < _last) ? _now - (_last - 1000) : _now - _last;
+            FPS = 1000.0d / _ms;
+            _last = _now;
             base.Update(gameTime);
-        }
-
-        private static ulong ConvertToTotalMilliseconds(DateTime time)
-        {
-            return ((ulong)time.Minute * 60000 + (ulong)time.Second * 1000 + (ulong)time.Millisecond);
         }
     }
 }
